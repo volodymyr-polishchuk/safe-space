ab7226e4d7e610920e9d031862d93d5c336b187a
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-05-25 15:58:05 +0300

    Create header in dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: TranslateService) { }
 
   ngOnInit() {
   }
 
+  selectLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
